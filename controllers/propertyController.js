8e6a0c3c310e48b8c7df5af4646fe815453d64cb
Victor Grycajuk <victorgryca@gmail.com>
2025-06-06 20:29:42 -0300

    fix: fixes agencie name in properties view

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -2,8 +2,11 @@ const Property = require('../models/property');
 
 exports.index = async (req,res,next)=>{
   const { agencyId } = req.params;
+  // Busca o nome da agência
+  const agencyResult = await require('../config/db').query('SELECT name FROM agencies WHERE id = $1', [agencyId]);
+  const agencyName = agencyResult.rows[0] ? agencyResult.rows[0].name : agencyId;
   const properties = await Property.findAll(agencyId);
-  res.render('property/index', { agencyId, properties });
+  res.render('property/index', { agencyId, agencyName, properties });
 };
 
 exports.store   = async (req,res,next)=>{
