bb027cf822a228a8b954803d181ba9cbeccd22b2
Victor Grycajuk <victorgryca@gmail.com>
2025-06-08 17:12:16 -0300

    fix: back without client screen

--- routes/client.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router({ mergeParams: true });
-const ctrl = require('../controllers/clientController');
-
-router.get('/calendar/:brokerId/:propertyId', ctrl.calendar);
-
-module.exports = router;
